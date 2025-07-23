28a12ace57d86f5640bcfd9934e97c597daf75fe
miki_hayashi <>
2025-07-23 10:50:42 +0900

    編集画面で現在の画像を表示

--- resources/views/diaries/edit.blade.php
+++ resources/views/diaries/edit.blade.php
@@ -7,6 +7,13 @@
     <form action="{{ route('diaries.update', $diary) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
+        {{-- 画像プレビュー --}}
+        @if ($diary->image_path)
+        <div class="mb-4">
+            <p class="text-sm text-gray-600">現在の画像:</p>
+            <img src="{{ asset('storage/' . $diary->image_path) }}" alt="日記画像" class="w-64 h-auto mt-2 rounded border">
+        </div>
+        @endif
         @include('diaries.form', ['diary' => $diary])
         <div class="flex justify-between items-center pt-4">
             <a href="{{ route('diaries.index') }}" class="text-sm text-blue-600 hover:underline">← 一覧に戻る</a>
